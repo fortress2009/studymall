81e3514c58eb31e58183006f3072ddee422650ff
fortress2009 <zlorsb@163.com>
2020-08-01 16:52:27 +0800

    第一次提交

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<template>
+  <div id="app">
+    <test></test>
+    <router-view />
+  </div>
+</template>
+<script>
+import test from "@/components/test.vue";
+
+export default {
+  name: "App",
+  components: {
+    test,
+  },
+};
+</script>
+
+<style>
+</style>
